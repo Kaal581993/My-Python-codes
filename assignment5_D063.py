6342de61ac969625147601c1214e9f0529cf68ee
Kaal581993 <viral.prajapati.1993@gmail.com>
2017-03-10 17:27:48 +0530

    Update assignment5_D063.py

--- assignment5_D063.py
+++ assignment5_D063.py
@@ -1,15 +1,11 @@
-Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 10+15
+
+
+10+15
 25
->>> print("Hello World")
-Hello World
->>> 45-34
+print("Hello World")
+45-34
 11
->>> 8*2
+8*2
 16
->>> print("Rahul's age is",45)
-Rahul's age is 45
->>> print("I have",10,"mangoes and",12,"bananas")
-I have 10 mangoes and 12 bananas
->>> 
+ print("Rahul's age is",45)
+print("I have",10,"mangoes and",12,"bananas")
