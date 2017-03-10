9d074cf7eed52ba250f9bf4859299f6fccb6fe50
Kaal581993 <viral.prajapati.1993@gmail.com>
2017-03-10 17:31:25 +0530

    Update assignment7_D063.py

--- assignment7_D063.py
+++ assignment7_D063.py
@@ -1,25 +1,19 @@
-Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> customer_id=101
->>> type(customer_id)
-<class 'int'>
->>> customer_name="John"
->>> type(customer_name)
-<class 'str'>
->>> bill_amount=675.45
->>> type(bill_amount)
-<class 'float'>
->>> x=5.3+0.9j
->>> type(x)
-<class 'complex'>
->>> print(customer_id,customer_name,bill_amount)
-101 John 675.45
->>> print(x.real)
-5.3
->>> print(x.imag+3)
-3.9
->>> Flag=True
->>> type(Flag)
-<class 'bool'>
->>> y="False"
+
+ customer_id=101
+ type(customer_id)
+
+ customer_name="John"
+ type(customer_name)
+
+ bill_amount=675.45
+ type(bill_amount)
+
+ type(x)
+ print(customer_id,customer_name,bill_amount)
+
+ print(x.real)
+ print(x.imag+3)
+ Flag=True
+ type(Flag)
+ y="False"
 
