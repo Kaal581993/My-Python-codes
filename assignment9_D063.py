f141d5c8b52fc3467aa36870b1974bbfa4f111a2
Kaal581993 <viral.prajapati.1993@gmail.com>
2017-03-10 17:33:25 +0530

    Update assignment9_D063.py

--- assignment9_D063.py
+++ assignment9_D063.py
@@ -1,19 +1,11 @@
-Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> i=10 #creates an integer variable. This is a single comment.
->>> print("i=",i) #prints 10
+ i=10 #creates an integer variable. This is a single comment.
+ print("i=",i) #prints 10
 i= 10
->>> '''
-Below code creates a Boolean variable in Python
 
-(This is a multple line comment )
-
-'''
-'\nBelow code creates a Boolean variable in Python\n\n(This is a multple line comment )\n\n'
->>> s=True
->>> print("s=",s) #prints True, Here, s is a boolean variable with value True
+s=True
+print("s=",s) #prints True, Here, s is a boolean variable with value True
 s= True
->>> """
+"""
 Below code assigns string data to variable 's'. Data type of variable can change during execution,
 
 Hence, Pytohn supports Dynamic Semantics.
@@ -22,7 +14,7 @@ Hence, Pytohn supports Dynamic Semantics.
 
 """
 "\nBelow code assigns string data to variable 's'. Data type of variable can change during execution,\n\nHence, Pytohn supports Dynamic Semantics.\n\n(This is multi-line comment used for documentation)\n\n"
->>> s=24
->>> print("s=",s) #prints 24, Here, s is changed to integer data type with value 24#
+ s=24
+ print("s=",s) #prints 24, Here, s is changed to integer data type with value 24#
 s= 24
->>> 
+ 
